a21058e680205189c53c81b9a4ba67f99faa553a
Sam <sam.sutcliffe2@gmail.com>
2019-11-15 16:47:12 +0000

    Route analysis improvement - new route & plots

--- FEASIBILITY STUDY/Battery/RouteAnalysis/route_analysis.py
+++ FEASIBILITY STUDY/Battery/RouteAnalysis/route_analysis.py
@@ -3,19 +3,30 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import filters 
 #Convert google maps to GPX via http://www.gpsies.com/convert.do
 import gpxpy
-with open('AH-route.gpx', 'r') as gpx_file:
-    gpx = gpxpy.parse(gpx_file)
-height_data = np.zeros((len(gpx.get_points_data())))
-distance_data = np.zeros((len(gpx.get_points_data())))
+with open('AH-route.gpx', 'r',encoding='utf-8') as gpx_file:
+    gpx_string = str(gpx_file.read())
+    gpx = gpxpy.parse(gpx_string)
+height_data = np.zeros((len(gpx.get_points_data())), dtype=np.float)
+distance_data = np.zeros((len(gpx.get_points_data())), dtype=np.float)
 for i,p in enumerate(gpx.get_points_data()):
-    height_data[i] = p.point.elevation
-    distance_data[i] = p.distance_from_start
+    height_data[i] = float(p.point.elevation)
+    distance_data[i] = float(p.distance_from_start)
 #pos_data = distance_data[:]
 #pos_data[1:] = pos_data[1:] - pos_data[:-1]
 #pos_data[0] = 0
 grad = np.gradient(height_data,distance_data)
 plt.figure()
 plt.plot(distance_data,height_data)
+plt.title("Elevation of the AH's example route")
+plt.xlabel("Distance (m)")
+plt.ylabel("Elevation (m)")
+plt.savefig("elevation.png")
 plt.figure()
-plt.plot(filters.gaussian_filter(grad,5))
+plt.title("Gradient of the AH's example route")
+plt.plot(distance_data,grad*100,label="Raw")
+plt.plot(distance_data,filters.gaussian_filter(grad,5)*100,label="Filtered")
+plt.xlabel("Distance (m)")
+plt.ylabel("Gradient (%)")
+plt.legend()
+plt.savefig("grad.png")
 plt.show()
